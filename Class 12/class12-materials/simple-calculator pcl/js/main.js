9871c6f92fe665e6ab22298c248a67c6bdc7cafd
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-04-11 18:42:40 -0600

    Not so great calculator

--- Class 12/class12-materials/simple-calculator pcl/js/main.js
+++ Class 12/class12-materials/simple-calculator pcl/js/main.js
@@ -1,26 +1,50 @@
+// Setup
 let total = 0
+let symbol = ''
+console.log('total = ' + total)
+console.log('symbol = ' + symbol)
 
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#dominosPizza').addEventListener('click', jumanji)
-document.querySelector('#zebra').addEventListener('click', add9)
-document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+// Addition & Subtraction
+document.querySelector('#numPlus').addEventListener('click', add)
+document.querySelector('#numMinus').addEventListener('click', sub)
 
-function makeZero() {
-  total = 0
-  document.querySelector('#placeToPutResult').innerText = total
+// Numbers
+document.querySelector('#num1').addEventListener('click', function() {num(1);})
+document.querySelector('#num2').addEventListener('click', function() {num(2);})
+document.querySelector('#num3').addEventListener('click', function() {num(3);})
+document.querySelector('#num4').addEventListener('click', function() {num(4);})
+document.querySelector('#num5').addEventListener('click', function() {num(5);})
+document.querySelector('#num6').addEventListener('click', function() {num(6);})
+document.querySelector('#num7').addEventListener('click', function() {num(7);})
+document.querySelector('#num8').addEventListener('click', function() {num(8);})
+document.querySelector('#num9').addEventListener('click', function() {num(9);})
+
+// Function to add or subtract
+function num(x) {
+  if (symbol === '+') {
+    total += x;
+    console.log('total = ' + total);
+    console.log('symbol = ' + symbol)
+    document.querySelector('#displayResults').innerHTML = total
+  } else if (symbol === '-') {
+    total -= x;
+    console.log('total = ' + total);
+    console.log('symbol = ' + symbol)
+    document.querySelector('#displayResults').innerHTML = total
+  } else {
+    console.log('No symbol attached');
+  }
+
 }
 
-function jumanji() {
-  total = total + 3
-  document.querySelector('#placeToPutResult').innerText = total
+function add() {
+  symbol = '+';
+  console.log( 'Symbol is now +');
+  console.log(symbol);
 }
 
-function add9() {
-  total = total + 9
-  document.querySelector('#placeToPutResult').innerHTML = total
-}
-
-function sub2() {
-  total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
+function sub() {
+  symbol = '-';
+  console.log( 'Symbol is now -');
+  console.log(symbol);
 }
