b9eec69e16e52109482928fbb5dc529253c0b380
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-04-30 11:01:15 -0600

    updated directory

--- Class 14/class14-materials/template/js/main.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello World')
-
-const myElements = document.querySelectorAll('.myClass');
-myElements.forEach(element => {element.addEventListener('click', run);});
-
-function run() {
-    console.log('Hello Again')
-}
-
