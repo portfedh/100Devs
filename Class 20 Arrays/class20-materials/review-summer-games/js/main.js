7f6131ca9f581bbd2c860a0741a8d79ac1e4ae84
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-05-07 10:00:20 -0600

    Class 20 Exercises

--- Class 20 Arrays/class20-materials/review-summer-games/js/main.js
+++ Class 20 Arrays/class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,21 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+
+function multiplyMe(array){
+    let product = 1;
+    for (i=0; i < array.length; i++) {
+        product = product * array[i]
+    }
+    alert(product)
+}
+
+multiplyMe([2, 3, 4, 5])
+
+
+// Same function but with for each
+function multiplyMe2(array){
+    let product = 1;
+    array.forEach( number => product *= number)
+    alert(product)
+}
+
+multiplyMe2([2, 3, 4, 5])
--- Class 20 Arrays/class20-materials/review-winter-games/js/main.js
+++ Class 20 Arrays/class20-materials/review-winter-games/js/main.js
@@ -1,1 +1,17 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+//Create a function that takes in an array of numbers.
+// Return a new array containing every even number from the original array (do not use map or filter)
+
+
+function evenNumber(array){
+    let newArray = [];
+
+    for (i=0; i < array.length; i++)
+        if (array[i] % 2 == 0) {
+            newArray.push(array[i])
+        }
+    
+    console.log(newArray)
+    return newArray;
+};
+
+evenNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
