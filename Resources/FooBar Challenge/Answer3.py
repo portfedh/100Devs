0c6e61e027c65e06d2588eb0cd2e2f942f70e6e5
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-05-09 15:10:12 -0600

    First draft of google foobar answer

--- Resources/FooBar Challenge/Answer3.py
+++ Resources/FooBar Challenge/Answer3.py
@@ -1,17 +1,41 @@
 def solution(x, y):
-    mach = int(x)
-    fecula = int(y)
-    number_generations = 0
+    # Convert strings into integers
+    m = int(x)
+    f = int(y)
+    # Set the number of generations
+    gens = 0
+    # Start inverse replication loop
     while True:
-        if mach == 1:
-            if fecula == 1:
-                return str(number_generations)
-        elif mach<1 or fecula<1 or mach==fecula:
+        # If M and F are equal to 1
+        # return the number of generation passed
+        if m == 1 and f == 1:
+            return str(gens)
+        # If either M or F is less than 1
+        # or they are equal (but not 1, it'd be
+        # captured above) return "impossible"
+        elif m<1 or f<1 or m==f:
             return "impossible"
+        # Check which one is larger, perform
+        # the inverse replication and increment
+        # the generation counter
         else:
-            if mach > fecula:
-                mach -= fecula
-                number_generations += 1
+            if m > f:
+                # If the difference is more 
+                # than 10x, calculate a 
+                # multiplication factor and
+                # increase gens accordingly 
+                if m > 10*f:
+                    multi = (int(m/f) -1)
+                    gens += multi
+                    m = m - (multi*f)
+                else:
+                    m-=f
+                    gens += 1
             else:
-                fecula -= mach
-                number_generations += 1
+                if f > 10*m:
+                    multi = (int(f/m) -1)
+                    gens += multi
+                    f = f - (multi*m)
+                else:
+                    f-=m
+                    gens += 1
