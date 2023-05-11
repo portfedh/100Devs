58b70adf5e1c528a1ec4ee8ba9ed667c89573f20
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-05-11 07:49:24 -0600

    Foobar Submission

--- Resources/FooBar Challenge/04 Answer3a.py
+++ Resources/FooBar Challenge/04 Answer3a.py
@@ -1,7 +1,5 @@
-Last Update
 
-    08 Mayo 2023
-
+'''
 "Bomb, Baby!"
 
 Constraints
@@ -54,3 +52,54 @@ Input:
 solution.solution('2', '1')
 Output:
     1
+'''
+
+def solution(match_bombs_needed, facula_bombs_needed):
+
+    # Convert input to integers
+    match_bomb = int(match_bombs_needed)
+    facula_bomb = int(facula_bombs_needed)
+
+    # Keep track of generations
+    generations_needed = 0
+
+    # Start inverse replication loop
+    while True:
+        # Case: End of loop --> Ok
+        # If match_bomb and facula_bomb are equal to 1
+        # We are at the start of the the loop.
+        # Return the number of generation passed as a string
+        if match_bomb == 1 and facula_bomb == 1:
+            return str(generations_needed)
+        
+        # Case: End of loop --> Impossible
+        elif match_bomb <= 0 or facula_bomb <= 0:
+            return "impossible"
+        
+        # Case: Inverse replication loop
+        # Check which bomb is larger
+        # Subtract the number of bombs with the smaller number from the larger number
+        # This will get us to the state of bombs in the previous generation
+        # Increment the generation counter
+        else:
+            if match_bomb > facula_bomb:
+                # If the difference is more than 5x, 
+                # Calculate a multiplication factor
+                # This is to reduce the number of loops needed
+                # If the bomb number is high
+                # And increase generations by result 
+                if match_bomb > (5 * facula_bomb):
+                    multiplicationFactor = (int(match_bomb / facula_bomb) -1)
+                    generations_needed += multiplicationFactor
+                    match_bomb = match_bomb - (multiplicationFactor * facula_bomb)
+                else:
+                    match_bomb -= facula_bomb
+                    generations_needed += 1
+            else:
+                if facula_bomb > (5 * match_bomb):
+                    multiplicationFactor = (int(facula_bomb / match_bomb) -1)
+                    generations_needed += multiplicationFactor
+                    facula_bomb = facula_bomb - (multiplicationFactor * match_bomb)
+                else:
+                    facula_bomb -= match_bomb
+                    generations_needed += 1
