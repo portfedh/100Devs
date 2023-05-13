496cbc828d288829186e2ed650bd0603e61879ec
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-05-12 23:24:48 -0600

    Array Exercices

--- Class 19 Arrays/Homework/EloquentJs.js
+++ Class 19 Arrays/Homework/EloquentJs.js
@@ -1,6 +1,5 @@
 // https://eloquentjavascript.net/04_data.html
 
-
 /*
 Task 1: The sum of a range
 
@@ -9,6 +8,7 @@ and returns an array containing all the numbers from start up to (and including)
 
 Next, write a sum function that takes an array of numbers and returns the sum of these numbers.
 */
+console.log("Task 1: The sum of a range");
 
 function rangeArray(startNumber, endNumber, step=1){
     if (step >= 0) {
@@ -45,3 +45,64 @@ console.log(theArray);
 
 sumTotal = sumArray(theArray)
 console.log(sumTotal);
+
+/*
+Task 2: Reversing an array
+
+ For this exercise, write two functions, reverseArray and reverseArrayInPlace. 
+ The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. 
+ 
+ The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements.
+ Neither may use the standard reverse method.
+
+ console.log(reverseArray(["A", "B", "C"]));
+ // → ["C", "B", "A"];
+
+*/
+console.log("Task 2: Reversing an array");
+
+function reverseArray(array){
+    
+    tempArray = [...array]
+    outputArray = [];
+
+    for(i=0; i < array.length; i++){
+        x = tempArray.pop()
+        outputArray.push(x)
+    }
+
+    console.log(outputArray)
+}
+
+
+myArray = ["A", "B", "C"]
+console.log(myArray)
+reverseArray(myArray)
+
+// Reversing an array in place
+// Modify the array given as argument by reversing its elements. 
+console.log('Task 2: Reversing an array in place')
+
+function reverseArrayInPlace(array){
+    let start = 0;
+    let end = array.length - 1;
+    while (start < end) {
+      // Swap elements
+      let temp = array[start];
+      array[start] = array[end];
+      array[end] = temp;
+      // Move the pointers
+      start++;
+      end--;
+    }
+    return array;
+  }
+
+myArray3 = ["A", "B", "C"]
+// ["C", "B", "A"]
+myArray4 = ["A", "B", "C", "D"]
+// ["D", "C", "B", "A"]
+
+console.log(myArray3)
+console.log(reverseArrayInPlace(myArray3))
+
