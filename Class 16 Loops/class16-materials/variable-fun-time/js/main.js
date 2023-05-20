06871b420c57733287d9a4fb92db765c3ab2b960
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-05-20 13:19:14 -0600

    Class 16 Final Exercise

--- Class 16 Loops/class16-materials/variable-fun-time/js/main.js
+++ Class 16 Loops/class16-materials/variable-fun-time/js/main.js
@@ -4,16 +4,18 @@ let myNumber = 36;
 
 //minus 10 from that number
 myNumber -= 10;
+// --> 26
 
 //print that number to the console
 console.log(myNumber)
 
 //--- Medium
 //create a variable that holds a value from the input
-let userValue = document.querySelector('#danceDanceRevolution').value
+let userValue = document.querySelector('#inputText').value
 
-//add 25 to that number
-userValue += 25;
+// Write 30 as an input value
+// add 25 to that number
+userValue = Number(userValue) + 25;
 
 //alert that number
 alert(userValue);
@@ -22,11 +24,12 @@ alert(userValue);
 //create a variable that holds the h1
 let myHeading = document.querySelector('#theHeading')
 
-//add an event listener to that element that console logs the sum of the two previous variables
+//add an event listener to that element 
+// that console logs the sum of the two previous variables
 myHeading.addEventListener('click', sumFunction);
 
 function sumFunction(){
     let subtotal = Number(myNumber) + Number(userValue)
-    console.log(subtotal)
+    console.log( `Subtotal is: ${subtotal} `)
     return subtotal
 }
