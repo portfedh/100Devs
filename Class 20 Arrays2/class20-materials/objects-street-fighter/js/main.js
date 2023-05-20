b8ff137996c6c128ad460c95a44248045dab2d1e
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-05-20 15:30:09 -0600

    Class 20 Exercises

--- Class 20 Arrays2/class20-materials/objects-street-fighter/js/main.js
+++ Class 20 Arrays2/class20-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,26 @@
-//Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+// Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function streetFighter(name, sex, power, life){
+    this.name = name;
+    this.sex = sex;
+    this.power = Number(power);
+    this.life = Number(life);
+
+    this.damage = function() {
+        this.power -= 10;
+    }
+
+    this.die = function(){
+        this.life = 0
+        console.log('You dead')
+    }
+
+    this.powerUp = function(){
+        this.power += 1000;
+    }
+}
+
+// Test Methods
+let Ryu = new streetFighter('Ryu', 'Male', 1000, 100);
+console.log(Ryu)
+Ryu.die()
+console.log(Ryu)
