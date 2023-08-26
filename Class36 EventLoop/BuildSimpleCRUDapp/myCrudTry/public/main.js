76ebdf560c0e7b12f4eae4e5320dbf57ae46f295
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-08-26 15:56:48 -0600

    Improve CRUD notes

--- Class36 EventLoop/BuildSimpleCRUDapp/myCrudTry/public/main.js
+++ Class36 EventLoop/BuildSimpleCRUDapp/myCrudTry/public/main.js
@@ -1,34 +1,38 @@
 // Greeting
-console.log("main.js from the Public folder running")
+console.log("publiv/main.js in now running.")
 
-
-// Listen for DarthVader Click:
+// Listen for DarthVader Update click:
 const update = document.querySelector('#update-button')
-
-// Area to write message if Darth Vader quote is not here
+// Area to write message if Darth Vader quote is not here:
 const messageDiv = document.querySelector('#message')
+// Listen for Delete click:
+const deleteButton = document.querySelector('#delete-button')
 
+// PUT Request using JavaScript:
+// (You can also create them with form elements)
 update.addEventListener('click', _ => {
-  // Send PUT Request here
   fetch('/quotes', {
+    // Tells server its sending a PUT request:
     method: 'put',
+    // Tells server its sending JSON data:
     headers: { 'Content-Type': 'application/json' },
+    // Convert data we are sending to JSON
     body: JSON.stringify({
-        name: 'Darth Vader',
-        quote: 'I find your lack of faith disturbing.',
+      name: 'Darth Vader',
+      quote: 'I find your lack of faith disturbing.'
     })
+  })
     .then(res => {
+      // Transform the response to JSON
       if (res.ok) return res.json()
     })
     .then(response => {
-      if (response === 'No quote to delete') {
-        messageDiv.textContent = 'No Darth Vader quote to delete'
-      } else {
-        window.location.reload(true)
-      }
+      // Refresh the browser to see the changes
+      window.location.reload(true)
+      // In a more complicated app you would use Js to update the DOM
+      // Without a reload.
     })
-    })
-  })
+})
 
 
 
--- Class36 EventLoop/BuildSimpleCRUDapp/myCrudTry/server.js
+++ Class36 EventLoop/BuildSimpleCRUDapp/myCrudTry/server.js
@@ -1,100 +1,120 @@
 // ***********
-// ** SETUP **
+// ** Imports **
 // ***********
-console.log('May Node be with you')
-
-// Imports
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-// Set template engine EJS
-app.set('view engine', 'ejs')
+console.log('server.js starting...')
 
-// Make public folder accessible to the public
-app.use(express.static('public'))
+// ******************************
+// ** Connect to MongoDB Atlas **
+// ******************************
 
-// Make it able to Read JSON
-app.use(bodyParser.json())
-
-// Port for the server to listen
-app.listen(3000, function () {console.log('listening on 3000')})
-
-// Handle html form elements
-app.use(bodyParser.urlencoded({ extended: true }))
-// Express cant read data from html forms
-// We install and use add bodyParser for that
-// Must come before the CRUD handlers
-
-// Connect to MongoDB Atlas
-
-// Connection information
+// Connection information:
+// =======================
 let connectionString = 'mongodb+srv://yoda:oNEn6lWuO2ADugjM@cluster0.tbcbcmb.mongodb.net/?retryWrites=true&w=majority'
+// Version using environment variables:
+//  @see https://zellwk.com/blog/environment-variables/
+//  require('./dotenv')
+//  Replace process.env.DB_URL with your actual connection string
+//  const connectionString = process.env.DB_URL
 
-// Connection code:
+// Establishing Connection:
+// =======================
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
-
-    // Connection message
+    // ========================
+    // Database and table info:
+    // ========================
+    // Connection confirmation  message
     console.log('Connected to Database');
-
     // DB name
     const db = client.db('star-wars-quotes');
+    // Table name (collection name)
+    const quotesCollection = db.collection('quotes');
 
-    // Table (collection)
-    const quotesCollection = db.collection('quotes');  
+    // ============================================
+    // Middleware (must come before CRUD Handlers)
+    // ============================================
+    // Set template engine EJS
+    app.set('view engine', 'ejs')
+    // Makes server able to Read JSON data
+    app.use(bodyParser.json())
+    // So express can read data from html elements
+    app.use(bodyParser.urlencoded({ extended: true }))
+    // Make public folder accessible to the public
+    app.use(express.static('public'))
+
+    // ========================
+    // Routes
+    // ========================
     
-    // Serve Index
+    // READ: Serve Index:
+    // ==================
     app.get('/', (req, res) => {
       // Gets quotes from database
-      db.collection('quotes')
-      .find()
-      .toArray()
-      .then(results => {
-        console.log(results)
-        // Rendered page with EJS and database results
-        res.render('index.ejs', { quotes: results })
+      db.collection('quotes').find().toArray()
+        .then(results => {
+          console.log(results)
+          // Rendered page with EJS and database results
+          res.render('index.ejs', { quotes: results })
+          // Alternative: serve index.html
+          // res.sendFile(__dirname + '/index.html')
+          //  What you would use for a static site.
       })
-      .catch(error => console.error(error))
-
-      // Serve index.html
-      // res.sendFile(__dirname + '/index.html')
-      // What you would use for a static site.
+        // Error handling
+        .catch(error => console.error(error))
       })
 
-    // Sending form info to mongoDB
+    // CREATE: Update MongoDB:
+    // =======================
+    // When server receives POST request from form.
     app.post('/quotes', (req, res) => {
-      quotesCollection
-        .insertOne(req.body)
+      // Insert record into database
+      quotesCollection.insertOne(req.body)
         .then(result => {
+          // Log what was inserted to server console
           console.log(result)
-          // Browser expects something back so we redirect back to home
+          // Browser expects something back so redirect back to home
           res.redirect('/')
         })
-
-        // Error handling in MongoDB
+        // Error handling
         .catch(error => console.error(error))
     })
     
-    // Updates the App
+    // UPDATE: Update MongoDB record
+    // =============================
+        // A less complicated process can be done with Mongoose. 
+        // Read tutorial here:
+        // https://zellwk.com/blog/mongoose/
+    // Responds to PUT request from public/main.js
     app.put('/quotes', (req, res) => {
+      // Method included in MongoDB
       quotesCollection.findOneAndUpdate(
+        // Query posts written by 'Yoda'
         { name: 'Yoda' },
         {
+          // Update operator $set
+          // Other possible are: $inc, $push
           $set: {
             name: req.body.name,
             quote: req.body.quote,
           },
         },
         {
+          // Create quote if no 'Yoda' quote exists
+          // upsert: insert if no documents can be updated
           upsert: true,
-        }
-      )
+        })
       .then(result => {
+        // Console log result in server
         console.log(result)
+        // Respond with Success JSON Message to client
         res.json('Success')
        })
+       // Error Handling
       .catch(error => console.error(error))
     })
 
@@ -111,7 +131,19 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
       .catch(error => console.error(error))
     })
 
-
+    // ========================
+    // Server Listen:
+    // ========================
+    // Port for the server to listen:
+    app.listen(3000, function () {
+      console.log('listening on 3000')
+    })
+    // Production version:
+    // const isProduction = process.env.NODE_ENV === 'production'
+    // const port = isProduction ? 7500 : 3000
+    // app.listen(port, function () {
+    //   console.log(`listening on ${port}`)
+    // })
     
   })
   // Error handling
