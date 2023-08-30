6b76d67dee4316139270d61287e82cc6ed9532af
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-08-30 13:42:14 -0600

    Cleaned Class36 Backend NodeJs Code

--- Class36 EventLoop/class36-materials/node-backend-simple-json/server.js
+++ Class36 EventLoop/class36-materials/node-backend-simple-json/server.js
@@ -1,93 +1,123 @@
 // Imports
+// *******
 const http = require('http');
 const fs = require('fs')
 const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 
+// Run Server
+// **********
 const server = http.createServer((req, res) => {
-  // Save requested url as variable
-  const page = url.parse(req.url).pathname;
-  console.log(page);
-  // Get the requested parameters sent with the url
-  const params = querystring.parse(url.parse(req.url).query);
-  console.log(params)
 
-  // Check endpoint: home
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  // Check endpoint: otherpage
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  // Check endpoint: otherotherpage
-  else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  // Check endpoint: api
-  // requires additional parameter: student
-  else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
-        }
-        res.end(JSON.stringify(objToJson));
+  // Functions:
+  // **********
+    // Render page:
+      function renderPage(pageName, contentType){
+        fs.readFile(pageName, function(err, data) {
+          res.writeHead(200, {'Content-Type': contentType});
+          res.write(data);
+          res.end();
+        })
       }
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-
-  // Serve css to html page to index.html
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  // Serve client side JScript to index.html
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  }else{
-    // What to serve if endpoint doesn't exist.
-    figlet('404!!', function(err, data) {
-      if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
+    // Page not found:
+      function render404(){
+        figlet('404!!', function(err, data) {
+          if (err) {
+              console.log('Something went wrong...');
+              console.dir(err);
+              return;
+          }
+          res.write(data);
+          res.end();
+        });
       }
-      res.write(data);
-      res.end();
-    });
-  }
-});
+    // Serve CSS:
+      function serveCSS(){
+        fs.readFile('css/style.css', function(err, data) {
+          res.write(data);
+          res.end();
+        });
+      }
+    // Serve JS:
+      function serveJs(){
+        fs.readFile('js/main.js', function(err, data) {
+          res.writeHead(200, {'Content-Type': 'text/javascript'});
+          res.write(data);
+          res.end();
+        });
+      }
+    // ServeAPI:
+      function serveAPI(){
+        if(params['student'] == 'leon'){
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          const objToJson = {
+            name: "leon",
+            status: "Boss Man",
+            currentOccupation: "Baller"
+          }
+          res.end(JSON.stringify(objToJson));
+        }
+        else {
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          const objToJson = {
+            name: "unknown",
+            status: "unknown",
+            currentOccupation: "unknown"
+          }
+          res.end(JSON.stringify(objToJson));
+        }
+      }
+  
+  // Variables
+  // *********
+    // Save requested url as variable:
+      const page = url.parse(req.url).pathname;
+        console.log(`The requested url is: "${page}"`);
 
-// Listen on port 8000
+    // Save any additional query parameters as a variable:
+      // Parameters are when the url has info attached in the url after '?'. 
+      // For example: /api?student=${userName}
+      // Here student is the query parameter.
+      const params = querystring.parse(url.parse(req.url).query);
+        console.log(`The params are: `)
+        console.log(params)
+
+  // Page controller
+  // ***************
+    switch(page){
+      
+      // Page loading
+      case '/css/style.css':
+        serveCSS();
+        break;
+
+      case '/js/main.js':
+        serveJs();
+        break;
+
+      // User requests
+      case '/':
+        renderPage('index.html', 'text/html');
+        break;
+
+      case '/otherpage':
+        renderPage('otherpage.html', 'text/html');
+        break;
+
+      case '/otherotherpage':
+        renderPage('otherotherpage.html', 'text/html');
+        break;
+
+      case '/api':
+        serveAPI();
+        break;
+
+      // 404
+      default:
+        render404();
+    }
+})
+
+// Port to listen for requests:
 server.listen(8000);
