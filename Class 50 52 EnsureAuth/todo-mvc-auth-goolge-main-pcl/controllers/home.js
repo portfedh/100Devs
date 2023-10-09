6f43087f3e9e624d74f93d67c78116db0f17bb3c
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-10-09 14:40:33 -0600

    Removed Extra file

--- Class 50 52 EnsureAuth/todo-mvc-auth-goolge-main-pcl/controllers/home.js
+++
@@ -1,8 +0,0 @@
-// Export the function so other scripts can import it
-module.exports = {
-    // Function named getIndex(request, response)
-    getIndex: (req,res)=>{
-        // When called, as a response, it renders index.ejs template
-        res.render('index.ejs')
-    }
-}
