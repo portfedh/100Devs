3e53cfe5e9171f1fc149ba5757250365d76badc6
Portfedh <59422723+portfedh@users.noreply.github.com>
2023-11-01 21:59:42 -0600

    Candela Update

--- Class 53 55/binary-upload-boom-main-pcl-edit/server.js
+++ Class 53 55/binary-upload-boom-main-pcl-edit/server.js
@@ -19,7 +19,7 @@ require("dotenv").config({ path: "./config/.env" });
 require("./config/passport")(passport);
 
 // Connect To Database
-connectDB();
+// connectDB();
 
 // Using EJS for views
 app.set("view engine", "ejs");
